4c9cf874433b42806e5f509e2cdfe6202f5d69d5
Shubham-2122 <shubhamjadav.tops@gmail.com>
2025-12-16 10:45:37 +0530

    react

--- react/demo/src/App.js
+++ react/demo/src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import ClassCompo from "./componenet/ClassCompo";
 
-function App(){
-    return(
-        <h1>Hello this App data</h1>
+function App() {
+    return (
+        <div>
+            <h1>Hello this App data</h1>
+            <ClassCompo />
+            <ClassCompo />
+        </div>
     )
 }
 export default App;
---
+++ react/demo/src/componenet/ClassCompo.js
@@ -0,0 +1,16 @@
+// componenet :- it's block of code when need we use it call.
+// two type : 1) class 2) function
+// class : Compoenent , reder and return 
+//  first letter capital 
+
+import React, { Component } from "react";
+
+class ClassCompo extends Component{
+    render(){
+        return(
+            <h1>Hello this class Compoenet</h1>
+        )
+    }
+}
+
+export default ClassCompo
